efc744101056ace3cd928d2b51d6012345d584ce
sloikodavid <sloikodavid@gmail.com>
2026-08-20 18:43:35 +0100

    Redirect from (auth) pages when signed in

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,4 +1,15 @@
-export default function AuthLayout({ children }: React.PropsWithChildren) {
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+import { routes } from "@/lib/routes";
+
+export default async function AuthLayout({
+	children,
+}: React.PropsWithChildren) {
+	const authentication = await auth();
+	if (authentication.isAuthenticated) {
+		redirect(routes.organizations.index);
+	}
+
 	return (
 		<main className="grid w-full flex-1 place-items-center px-4 py-8">
 			{children}
